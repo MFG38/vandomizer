b95011b57bb856ddab57caab2a6236863413b660
MFG38 <santtu.pesonen@mail.com>
2023-06-27 23:39:38 +0300

    Balance tweak #2

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/ env bash
-VERNUM="v11-pre-r102"
+VERNUM="v11-pre-r103"
 
 echo Building...
 7za a -tzip vandomizer_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
