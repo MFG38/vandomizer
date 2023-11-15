4d0a28716ae67b2ca34e8869ab60a30faa278f90
MFG38 <santtu.pesonen@mail.com>
2023-11-15 20:17:25 +0200

    Best not leave this unfinished.

--- _build.bat
+++ _build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERNUM=v11
+set VERNUM=v11-pre-r107
 
 echo Building...
 _tools\7za a -tzip vandomizer_%VERNUM%.pk3 *.* * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
