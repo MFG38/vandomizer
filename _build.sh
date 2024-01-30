e572cf36aad247e918cb9045e30b89b798397715
MFG38 <santtu.pesonen@mail.com>
2024-01-30 12:17:10 +0200

    v1.1 ready

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/ env bash
-VERNUM="v11-pre-r112"
+VERNUM="v11"
 
 echo Building...
 7za a -tzip vandomizer_${VERNUM}.pk3 * -x!.gitattributes -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
