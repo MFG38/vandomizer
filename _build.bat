eb98412735694749a1904c4cc7868f0c563bc1ba
MFG38 <santtu.pesonen@mail.com>
2024-06-18 14:39:39 +0300

    Further automation of the build system

--- _build.bat
+++ _build.bat
@@ -1,8 +1,12 @@
 @echo off
-set VERNUM=v12
+set /p VERNUM=<version
+
+echo [default] > language.version
+echo. >> language.version
+echo VD_VERSIONNUM = ^"%VERNUM%^"; >> language.version
 
 echo Building...
-_tools\7za a -tzip vandomizer_%VERNUM%.pk3 *.* * -x!.gitattributes -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
+_tools\7za a -tzip vandomizer_%VERNUM%.pk3 *.* * -x!.gitattributes -x!.gitignore -x!_build.bat -x!_build.sh -x!version -xr!.git -xr!_build -xr!_tools
 move vandomizer_%VERNUM%.pk3 _build/vandomizer_%VERNUM%.pk3
 goto Done
 
--- _build.sh
+++ _build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/ env bash
-VERNUM="v12"
+VERNUM=$(cat version)
 
-echo Building...
-7za a -tzip vandomizer_${VERNUM}.pk3 * -x!.gitattributes -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
+echo "[default]" > language.version
+echo "" >> language.version
+echo "VD_VERSIONNUM = \"$VERNUM\";" >> language.version
+
+echo "Building..."
+7za a -tzip vandomizer_${VERNUM}.pk3 * -x!.gitattributes -x!.gitignore -x!_build.bat -x!_build.sh -x!version -xr!.git -xr!_build -xr!_tools
 mv vandomizer_${VERNUM}.pk3 _build
